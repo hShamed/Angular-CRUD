5a0a92c955204fda93c342d1c65fd4d7f369fc38
hShamed <h.shamed.ar@gmail.com>
2023-12-15 19:12:35 -0600

    Desplegue del arreglo de empleados en el componente de Employees

--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Employee } from '../models/employee';
 
 @Component({
   selector: 'app-employees',
@@ -8,5 +9,10 @@ import { Component } from '@angular/core';
   styleUrl: './employees.component.css'
 })
 export class EmployeesComponent {
-
+  employeesArray: Employee[] = [
+    {id: 1, name: "Hiram Shamed Ambriz Rubio", country: "Mexico"},
+    {id: 2, name: "Scarlett Jimena Solórzano Rodríguez", country: "Mexico"},
+    {id: 3, name: "John Whick", country: "United States"},
+    {id: 4, name: "Xi Maotze", country: "China"},
+  ];
 }
